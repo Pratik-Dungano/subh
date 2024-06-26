a3e8af9bae112987309a9bbc391a88f56f41a6e1
nishi1403 <nimopanchal1417@gmail.com>
2024-06-27 01:46:25 +0530

    tech team

--- js/contri.js
+++ js/contri.js
@@ -1,53 +1,129 @@
 var teamMember = [{
-        name: 'Keval Vavaliya',
-        image: 'https://avatars.githubusercontent.com/u/72963356?v=4',
-        github: 'https://github.com/kevalvavaliya',
-        twitter: 'https://twitter.com/keval_vavaliya'
-    }, {
-        name: 'Nitesh Thapliyal',
-        image: 'https://avatars.githubusercontent.com/u/53345517?v=4dis',
-        github: 'https://github.com/Nitesh-thapliyal',
-        twitter: 'https://twitter.com/Bauddhik_Geek'
-    },
-    {
-        name: 'Aditya Mangal',
-        image: 'https://github.com/adityamangal1.png?size=460',
-        github: 'https://github.com/adityamangal1',
-        twitter: 'https://twitter.com/AdityaM44382015'
-    }, {
-        name: 'Piyush Chandel',
-        image: 'https://github.com/Piyush-Codes7.png?size=460',
-        github: 'https://github.com/Piyush-Codes7',
-        twitter: 'https://twitter.com/Piyush_Chandel7'
-    },
-    {
-        name: 'Harikrushn Kanani',
-        image: 'https://github.com/harikanani.png?size=460',
-        github: 'https://github.com/harikanani',
-        twitter: 'https://twitter.com/Harikrushn9'
-    }, {
-        name: 'Kamlesh Joshi',
-        image: 'https://github.com/kamleshjoshi8102.png',
-        github: 'https://github.com/kamleshjoshi8102',
-        twitter: 'https://twitter.com/spyd3r17'
-    },
-    {
-        name: 'Abhi Goyani',
-        image: 'https://github.com/abhigoyani.png',
-        github: 'https://github.com/abhigoyani',
-        twitter: 'https://twitter.com/abhigoyani_'
-    }, {
-        name: 'Raja Harsh Vardhan Singh',
-        image: 'https://github.com/rhvsingh.png',
-        github: 'https://github.com/rhvsingh',
-        twitter: 'https://twitter.com/rhvsingh'
-    }, {
-        name: 'Rajkumar Khistariya',
-        image: 'https://github.com/Rajkumar-justcoder.png',
-        github: 'https://github.com/Rajkumar-justcoder',
-        twitter: 'https://twitter.com/Rajkumar_THM'
-    }
-];
+    name: 'Rahul Khinchi',
+    image: '../images/team-images/leader.jpeg',
+    github: 'https://github.com/Rahulkhinchi03',
+    linkedin: 'https://www.linkedin.com/in/rahulkhinchi03/'
+},
+{
+    name: 'Shiv Vishen',
+    image: '../images/team-images/Shiv.jpg',
+    github: 'https://github.com/shvshn',
+    linkedin: 'http://www.linkedin.com/in/shivvishen'
+},
+{
+    name: 'Kshitij Varma',
+    image: '../images/team-images/kshitij.jpeg',
+    github: 'https://github.com/kstij',
+    linkedin: 'https://www.linkedin.com/in/kshitijvarma21/'
+},
+{
+    name: 'Harsh Trivedi',
+    image: '../images/team-images/harsh.jpeg',
+    github: 'https://github.com/Harsh-Trivedi03',
+    linkedin: 'https://www.linkedin.com/in/harsh-trivedi03/'
+},
+{
+    name: 'Reedham Pujara',
+    image: '../images/team-images/reedham.jpeg',
+    github: 'https://github.com/Reedham20',
+    linkedin: 'https://www.linkedin.com/in/reedham-pujara/'
+},
+{
+    name: 'Nishtha Pitroda',
+    image: '../images/team-images/nishtha.jpeg',
+    github: 'https://github.com/pitrodanishtha09',
+    linkedin: 'https://www.linkedin.com/in/nishthapitroda/'
+},
+{
+    name: ' Yagnesh Patoriya',
+    image: '../images/team-images/YAGNESH.jpg',
+    github: 'https://github.com/yaggit',
+    linkedin: 'https://www.linkedin.com/in/yagnesh-patoriya'
+},
+{
+    name: ' Parv Pareek',
+    image: '../images/team-images/parv.png',
+    github: 'https://github.com/parvpareek',
+    linkedin: 'https://www.linkedin.com/in/parvpareek'
+},
+{
+    name: ' Vaibhav Sharma',
+    image: '../images/team-images/vaibhav.jpg',
+    github: 'https://github.com/vaibhavvext',
+    linkedin: 'https://www.linkedin.com/in/vaibhav-sharma-84606b256/'
+},
+{
+    name: 'Virengiri Goswami',
+    image: '../images/team-images/virengiri.jpg',
+    github: 'https://github.com/Veerengiri',
+    linkedin: 'https://www.linkedin.com/in/virengiri-goswami/'
+},
+{
+    name: 'Sneha Singh',
+    image: '../images/team-images/sneha.jpg',
+    github: 'https://github.com/Sneha6003',
+    linkedin: 'https://www.linkedin.com/in/sneha-singh-636578273/'
+},
+{
+    name: '  Sahil Tiwari',
+    image: '../images/team-images/sahil.jpg',
+    github: 'https://github.com/sahil-1610',
+    linkedin: 'https://www.linkedin.com/in/sahil-tiwari-8a166926b'
+},
+{
+    name: '   Urmit Tajwala',
+    image: '../images/team-images/urmit.jpg',
+    github: 'https://github.com/URMIT18',
+    linkedin: 'https://www.linkedin.com/in/urmit-tajwala-5296a1221/'
+},
+{
+    name: '  Nidhi Dattani',
+    image: '../images/team-images/nidhi.jpg',
+    github: 'https://github.com/nidhidattani13',
+    linkedin: 'http://www.linkedin.com/in/nidhidattani'
+},
+{
+    name: ' Vidya sinha',
+    image: '../images/team-images/vidhya.jpg',
+    github: 'https://github.com/VidyaSinha',
+    linkedin: 'https://www.linkedin.com/in/vidya-sinha-88715224b?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3BH%2FzgEISCQm62Dpve%2FUuJJQ%3D%3D'
+},
+{
+    name: 'Nishi Panchal',
+    image: '../images/team-images/nishi.jpg',
+    github: 'https://github.com/nishi1403',
+    linkedin: 'https://www.linkedin.com/in/nishi-panchal-a09b64255'
+},
+{
+    name: ' Pratik Dungano ',
+    image: '../images/team-images/pratik.jpeg',
+    github: 'https://github.com/Pratik-Dungano',
+    linkedin: 'https://www.linkedin.com/in/pratik-dungano-9a6892250/'
+},
+{
+    name: 'Kenil Kavar ',
+    image: '../images/team-images/kenil.jpg',
+    github: 'https://github.com/Kenil-kavar',
+    linkedin: 'https://www.linkedin.com/in/kenil-kavar'
+},
+{
+    name: 'Khushal Khandelwal ',
+    image: '../images/team-images/khushal.jpg',
+    github: 'https://github.com/K-Khushal',
+    linkedin: 'https://www.linkedin.com/in/khushal-khandelwal/'
+},
+{
+    name: ' Dinesh Talwadker ',
+    image: '../images/team-images/dinesh.png',
+    github: 'https://github.com/dinxsh/',
+    linkedin: 'https://www.linkedin.com/in/dineshtalwadker/'
+},
+{
+    name: ' Darshan Gupta ',
+    image: '../images/team-images/darshan.png',
+    github: 'https://github.com/DDgupta07',
+    linkedin: 'https://www.linkedin.com/in/darshanguptatech/'
+},];
 
 var contriMember = [
     /* {
@@ -136,7 +212,7 @@ var contributors2 = document.querySelectorAll('.contributors')[1];
 for (let i = 0; i < teamMember.length; i++) {
     let divCreate = document.createElement('div');
     divCreate.className = 'contri_box';
-    divCreate.innerHTML = '<div class="contri-card"><div class="contri-card-inner"><div class="contri-card-front"><img src="' + teamMember[i].image + '" alt="' + teamMember[i].name + '" /></div><div class="contri-card-back"><div style="position:relative;transform-style:preserve-3d;"><span>' + teamMember[i].name + '</span><div> <li><a href="' + teamMember[i].github + '" target="_blank"><i class="fab fa-github"></i></a></li><li><a href="' + teamMember[i].twitter + '" target="_blank"><i class="fab fa-twitter"></i></a></li></div></div></div></div></div>';
+    divCreate.innerHTML = '<div class="contri-card"><div class="contri-card-inner"><div class="contri-card-front"><img src="' + teamMember[i].image + '" alt="' + teamMember[i].name + '" /></div><div class="contri-card-back"><div style="position:relative;transform-style:preserve-3d;"><span>' + teamMember[i].name + '</span><div> <li><a href="' + teamMember[i].github + '" target="_blank"><i class="fab fa-github"></i></a></li><li><a href="' + teamMember[i].linkedin + '" target="_blank"><i class="fab fa-linkedin"></i></a></li></div></div></div></div></div>';
     contributors1.append(divCreate);
 }
 
